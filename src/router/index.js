ee2e475aef5b18be971a53fae93bc3480964f459
Kheang-TE <kheang.te07@gmail.com>
2024-05-13 17:13:11 +0200

    feat: pages, routes, nav

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
+import SignUp from '../views/SignUp.vue'
+import SignIn from '../views/SignIn.vue'
+import NotFound from '../views/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +10,21 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/signup',
+      name: 'sign up',
+      component: SignUp
+    },
+    {
+      path: '/signin',
+      name: 'sign in',
+      component: SignIn
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      component: NotFound
     }
   ]
 })
