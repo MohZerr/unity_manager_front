ae6466cf9e19435049f9104a0c3cbbb1f43063e1
Kheang-TE <kheang.te07@gmail.com>
2024-05-14 17:01:40 +0200

    feat: signup page structure

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,6 +1,41 @@
 <template>
-  <div>
-    <h1>Sign Up</h1>
+  <div class="signup">
+    <header class="container">
+      <div class="row">
+        <div class="col">
+          <div class="logo_container">
+            <img src="/img/unity_manager_logo.webp" alt="Unity Manager" title="Unity Manager" class="logo">
+          </div>
+          <h1>Create account</h1>
+          <p>We will be happy to count you among us!</p>
+        </div>
+      </div>
+    </header>
+
+    <main class="container">
+      <div class="row">
+        <div class="col">
+          <div class="form-container">
+            <div class="form-header">
+              Please fill in all fields to register
+            </div>
+            <div class="form-body">
+              <form action="" method="POST">
+                <input type="text" class="form-control mb" name="lastname" placeholder="Nom">
+                <input type="text" class="form-control mb" name="firstname" placeholder="Prénom">
+                <input type="email" class="form-control mb" name="email"  placeholder="Email">
+                <input type="password" class="form-control mb" name="password" placeholder="Mot de passe">
+                <input type="password" class="form-control mb" name="confirm_password" placeholder="Confirmation du mot de passe">
+                <button type="submit" class="btn">Create account</button>
+              </form>
+            </div>
+            <div class="form-footer">
+              <a href="/login">Sign in<i class="fa-solid fa-arrow-right-long"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
