6b8e0de1a9cb350793e958b1042c56f429cd6e85
Kheang-TE <kheang.te07@gmail.com>
2024-05-14 23:59:54 +0200

    feat: create page signup and signin

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -3,7 +3,7 @@
     <header class="container">
       <div class="row">
         <div class="col">
-          <div class="logo_container">
+          <div class="logo-container">
             <img src="/img/unity_manager_logo.webp" alt="Unity Manager" title="Unity Manager" class="logo">
           </div>
           <h1>Create account</h1>
@@ -13,29 +13,43 @@
     </header>
 
     <main class="container">
-      <div class="row">
-        <div class="col">
+      <div class="row justify-content-center">
+        <div class="col-lg-7 col-">
           <div class="form-container">
             <div class="form-header">
               Please fill in all fields to register
             </div>
             <div class="form-body">
               <form action="" method="POST">
-                <input type="text" class="form-control mb" name="lastname" placeholder="Nom">
-                <input type="text" class="form-control mb" name="firstname" placeholder="Prénom">
-                <input type="email" class="form-control mb" name="email"  placeholder="Email">
-                <input type="password" class="form-control mb" name="password" placeholder="Mot de passe">
-                <input type="password" class="form-control mb" name="confirm_password" placeholder="Confirmation du mot de passe">
-                <button type="submit" class="btn">Create account</button>
+                <input type="text" class="form-control" name="lastname" placeholder="Lastname">
+                <input type="text" class="form-control" name="firstname" placeholder="Firstname">
+                <input type="email" class="form-control" name="email"  placeholder="Email">
+                <input type="password" class="form-control" name="password" placeholder="Password">
+                <input type="password" class="form-control" name="confirm_password" placeholder="Confirm password">
+                <a href="/signin" class="float-start"><i class="fa"><font-awesome-icon :icon="['fas', 'arrow-left']" /></i>Sign in</a>
+                <button type="submit" class="btn float-end">Create account</button>
               </form>
             </div>
             <div class="form-footer">
-              <a href="/login">Sign in<i class="fa-solid fa-arrow-right-long"></i></a>
+              <a href="/"><i class="fa"><font-awesome-icon :icon="['fas', 'house']" /></i>Back to homepage</a>
             </div>
           </div>
         </div>
       </div>
     </main>
+
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <p>Copyrights &copy; 2024</p>
+          </div>
+          <div class="col text-end">
+            <p>Powered by <a href="https://oclock.io/" target="_blank" title="O'clock">O'clock</a></p>
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
