f474a3095b485d84597def56c537830bf2969919
Kheang-TE <kheang.te07@gmail.com>
2024-05-17 11:10:35 +0200

    feat: board in progress

---
+++ src/components/boardComponents/Chat.vue
@@ -0,0 +1,40 @@
+<!-- src/components/Sidebar.vue -->
+<template>
+  <div class="offcanvas offcanvas-end" :class="{ show: isOpen }" tabindex="-1" style="visibility: visible;">
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title">Chat</h5>
+      <button type="button" class="btn-close" @click="closeChat"></button>
+    </div>
+    <div class="offcanvas-body">
+      <p>Contenu de la sidebar...</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isOpen: {
+      type: Boolean,
+      required: true
+    }
+  },
+  methods: {
+    closeChat() {
+      this.$emit('update:isOpen', false);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.offcanvas {
+  width: 250px;
+  max-width: 100%;
+  transform: translateX(-100%);
+  transition: transform 0.3s ease-in-out;
+}
+.offcanvas.show {
+  transform: translateX(0);
+}
+</style>
---
+++ src/components/boardComponents/DropdownUser.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-dropdown id="dropdown">
+    <template #button-content>
+    <font-awesome-icon :icon="['fas', 'circle-user']" />
+    </template>
+    <b-dropdown-item><font-awesome-icon :icon="['fas', 'circle-user']" /> JohnDoe@domain.com</b-dropdown-item>
+    <b-dropdown-item><font-awesome-icon :icon="['fas', 'gear']" /> Account Setting</b-dropdown-item>
+    <b-dropdown-divider></b-dropdown-divider>
+    <b-dropdown-item><font-awesome-icon :icon="['fas', 'right-from-bracket']" /> Sign out</b-dropdown-item>
+  </b-dropdown>
+</template>
+
+<script>
+  export default {
+    name: 'DropdownUser',
+  }
+</script>
