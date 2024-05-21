6e2bcd79571b11a3d8038ecfdcbda8dbd51ae7f6
Mohamed Zerrouki <mohamed.zerrouki@oclock.school>
2024-05-22 00:05:24 +0200

    feat: Implement fetch requests and socket.io interactions for project details retrieval and room joining

--- src/components/boardComponents/Sidebar.vue
+++ src/components/boardComponents/Sidebar.vue
@@ -1,11 +1,33 @@
 <!-- src/components/Sidebar.vue -->
 <template>
   <b-offcanvas id="sidebar" title="Projects" :backdrop="false">
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        <a href="#" @click.prevent=fetchProject(project.id)>{{ project.name }}</a>
+      </li>
+    </ul>
   </b-offcanvas>
 </template>
 
 <script>
+import { getProjects, getProject } from '@/api/project.js';
+
 export default {
   name: 'Sidebar',
-}
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  async created() {
+    this.projects = await getProjects();
+  },
+  methods: {
+    fetchProject(id) {
+      getProject(id).then((response) => {
+        console.log(response);
+      });
+    },
+  },
+};
 </script>
