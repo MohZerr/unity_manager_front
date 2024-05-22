ba6fb7f480d5902c190399f3a8152a32e305f875
Kheang-TE <kheang.te07@gmail.com>
2024-05-23 00:21:12 +0200

    style: project sidebar

--- src/components/boardComponents/Sidebar.vue
+++ src/components/boardComponents/Sidebar.vue
@@ -1,9 +1,44 @@
 <!-- src/components/Sidebar.vue -->
 <template>
   <b-offcanvas id="sidebar" title="Projects" :backdrop="false">
-    <ul>
-      <li v-for="project in projects" :key="project.id">
-        <a href="#" @click.prevent=selectProject(project.id)>{{ project.name }}</a>
+    <template #title>
+      Projects
+
+      <!-- Create a new project -->
+      <b-button v-b-modal.new-project class="add-new-project">
+        <font-awesome-icon :icon="['fas', 'plus']" />
+      </b-button>
+      <b-modal id="new-project" title="Create new project" centered>
+      </b-modal>
+
+    </template>
+    <ul class="projects-list">
+      <li v-for="project in projects" :key="project.id" class="project-item">
+        <a href="#" class="project-link" @click.prevent=selectProject(project.id)>{{ project.name }}</a>
+        <div class="project-control">
+
+          <!-- Edit the project -->
+          <b-button v-b-modal="'edit-project-' + project.id" class="project-control-edit">
+            <font-awesome-icon :icon="['far', 'pen-to-square']" />
+          </b-button>
+          <b-modal :id="'edit-project-' + project.id" centered>
+            <template #title>
+              Edit the project : {{ project.name }}
+            </template>
+          </b-modal>
+
+          <!-- Delete the project -->
+          <b-button v-b-modal="'delete-project-' + project.id" class="project-control-delete">
+            <font-awesome-icon :icon="['far', 'trash-can']" />
+          </b-button>
+          <b-modal :id="'delete-project-' + project.id" centered>
+            <template #title>
+              Delete the project : {{ project.name }}
+            </template>
+            <p>Are you sure you want to delete this project ?</p>
+          </b-modal>
+
+        </div>
       </li>
     </ul>
   </b-offcanvas>
