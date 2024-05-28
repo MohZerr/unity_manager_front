db4bcc0da678ff668d3200469ad978df8fa903fd
Mohamed Zerrouki <mohamed.zerrouki@oclock.school>
2024-05-28 20:56:13 +0200

    feat: Implement localStorage to persist logged-in user data

--- src/store/user.store.js
+++ src/store/user.store.js
@@ -1,20 +1,27 @@
 import { defineStore } from 'pinia';
 
-export const useUserStore = defineStore('user', {
+const useUserStore = defineStore('user', {
   state: () => ({
-    user: {
+    // Define user object with values set to localStorage values if it exists or else set to an empty object
+    user: JSON.parse(localStorage.getItem('user')) || {
       id: null,
       firstname: '',
       lastname: '',
     },
   }),
   actions: {
+    persistToLocalStorage() {
+      localStorage.setItem('user', JSON.stringify(this.user));
+    },
     setUser(user) {
       this.user = {
         id: user.id,
         firstname: user.firstname,
         lastname: user.lastname,
       };
+      this.persistToLocalStorage();
     },
   },
 });
+
+export default useUserStore;
