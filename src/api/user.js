ab6bb8664a3ab346b4a58faff74bd323d49fc514
HENRIiosipov <henri.iosipov@oclock.school>
2024-05-29 18:43:00 +0200

    feat: accout setting

--- src/api/user.js
+++ src/api/user.js
@@ -1,5 +1,6 @@
 import axios from './axios.js';
 import { connecting } from '@/sockets/socket';
+import { useUserStore } from "@/store/user.store.js";
 
 export const signup = async (user) => {
   try {
@@ -19,13 +20,55 @@ export const signup = async (user) => {
 
 export const signin = async (email, password) => {
   try {
-    const response = await axios.post('users/signin', {
-      email,
-      password,
-    });
-    connecting(response.data.firstname);
-    return response.data;
+    const response = await axios.post("users/signin", { email, password });
+    const userData = {
+      id: response.data.id,
+      firstname: response.data.firstname,
+      lastname: response.data.lastname,
+      email: response.data.email,
+      code_color: response.data.code_color,
+    };
+    console.log("User data:", userData);
+    const userStore = useUserStore();
+    userStore.setUser(userData);
+    connecting(userStore.user);
+    return userData;
   } catch (error) {
+    console.error("Sign in failed:", error);
     throw error;
   }
 };
+
+export const updateUser = async (userData) => {
+  try {
+    console.log('userData : ', userData.id);
+    const response = await axios.patch(`/users/${userData.id}`, {
+      id: userData.id,
+      lastname: userData.lastname,
+      firstname: userData.firstname,
+      password: userData.password,
+      confirmation: userData.confirmation,
+      code_color: userData.code_color,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const signOut = async (userId) => {
+  try {
+    const response = await axios.get(`/users/${userId}`);
+    if (response.status === 200) {
+      return true;
+    } else {
+      console.error("Failed to sign out user: ", response.status);
+      return false;
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+ 
--- src/components/boardComponents/List.vue
+++ src/components/boardComponents/List.vue
@@ -129,14 +129,7 @@ export default {
           content: this.newCard.content,
           list_id: id,
         };
-
-        const createdCard = await createCard(newCard);
-
-        if (createdCard) {
-          this.card.push(createdCard);
-        } else {
-          console.error('Error creating the card');
-        }
+        await createCard(newCard);
       } catch (error) {
         console.error('Error creating the card :', error);
       }
