3c0906720c5e6e8a1f5f67115831994503252ff6
HENRIiosipov <henri.iosipov@oclock.school>
2024-05-29 18:43:00 +0200

    feat: accout setting

--- src/components/boardComponents/DropdownUser.vue
+++ src/components/boardComponents/DropdownUser.vue
@@ -4,28 +4,30 @@
       <font-awesome-icon :icon="['fas', 'circle-user']" />
     </template>
     <b-dropdown-item>
-      <font-awesome-icon :icon="['fas', 'circle-user']" /> JohnDoe@domain.com
+      <font-awesome-icon :icon="['fas', 'circle-user']" /> {{ store.user.lastname }} {{ store.user.firstname }}
     </b-dropdown-item>
     <b-dropdown-item>
       <b-link v-b-modal.account>
         <font-awesome-icon :icon="['fas', 'gear']" /> Account Setting
       </b-link>
-      <b-modal id="account" size="xl" title="Account Setting" centered>
+      <b-modal id="account" size="xl" title="Account Setting" centered @ok="submitUpdateUser">
         <b-accordion>
+          <b-form @submit.prevent="submitUpdateUser" >
           <b-accordion-item title="Profile" visible>
-            <label for="email"> Email :</label>
-            <b-form-input id="email" :type="text" v-model="email" disabled></b-form-input>
-            <label for="lastname"> Lastname</label>
-            <b-form-input id="lastname" :type="text" v-model="lastname"></b-form-input>
-            <label for="firstname"> Firstname</label>
-            <b-form-input id="firstname" :type="text" v-model="firstname"></b-form-input>
+            <label for="email">Email :</label>
+            <b-form-input id="email" type="text" v-model="store.user.email" disabled ></b-form-input>
+            <label for="lastname">Lastname:</label>
+            <b-form-input id="lastname" type="text" v-model="store.user.lastname"></b-form-input>
+            <label for="firstname">Firstname:</label>
+            <b-form-input id="firstname" type="text" v-model="store.user.firstname" ></b-form-input>
           </b-accordion-item>
           <b-accordion-item title="Change password">
-            <label for="password">Enter new password</label>
-            <b-form-input id="password" :type="password"></b-form-input>
-            <label for="confirm_password">Confirm new password</label>
-            <b-form-input id="confirm_password" :type="password"></b-form-input>
+            <label for="password">Enter new password:</label>
+            <b-form-input id="password" type="password" v-model="store.user.password"></b-form-input>
+            <label for="confirm_password">Confirm new password:</label>
+            <b-form-input id="confirm_password" type="password" v-model="store.user.confirmation"></b-form-input>
           </b-accordion-item>
+          </b-form>
         </b-accordion>
       </b-modal>
     </b-dropdown-item>
@@ -34,7 +36,7 @@
       <b-link v-b-modal.signout>
         <font-awesome-icon :icon="['fas', 'right-from-bracket']" /> Sign out
       </b-link>
-      <b-modal id="signout" centered>
+      <b-modal id="signout" centered @ok="signOutUser">
         <template #title>
           Sign out
         </template>
@@ -46,18 +48,52 @@
 
 <script>
 import { useUserStore } from '@/store/user.store.js';
+import { updateUser } from '@/api/user.js';
+import { signOut } from '@/api/user.js';
 
 export default {
   setup() {
     const store = useUserStore();
     return { store };
   },
-  data() {
-    return {
-      email: 'johnDoe@domain.com',
-      lastname: 'John',
-      firstname: 'Doe',
-    };
+  methods: {
+    async submitUpdateUser() {
+      try {
+        const userData = {
+          id: this.store.user.id,
+          lastname: this.store.user.lastname,
+          firstname: this.store.user.firstname,
+          password: this.store.user.password,
+          confirmation: this.store.user.confirmation,
+          code_color: this.store.user.code_color,
+        };
+        console.log('Updating user with ID:', userData.id);
+        const response = await updateUser(userData);
+        if (response) {
+          console.log('User updated successfully');
+        } else {
+          console.error('Failed to update user');
+        }
+      } catch (error) {
+        console.error('Error updating the user info:', error);
+      }
+    },
+
+    async signOutUser() {
+      try {
+        console.log('Sign out user with ID:', this.store.user.id);
+        const response = await signOut(this.store.user.id);
+        if (response === true) {
+          console.log('User signed out successfully');
+          this.store.$reset();          
+          this.$router.push({ name: 'sign in' });
+        } else {
+          console.error('Failed to sign out user');
+        }
+      } catch (error) {
+        console.error('Error signing out user:', error);
+      }
+    },
   },
 };
 </script>
--- src/store/user.store.js
+++ src/store/user.store.js
@@ -1,15 +1,48 @@
-import { defineStore } from 'pinia';
+import { defineStore } from "pinia";
 
-export const useUserStore = defineStore('user', {
+export const useUserStore = defineStore("user", {
   state: () => ({
-    user: {
+    user: JSON.parse(localStorage.getItem("user")) || {
       id: null,
-      pseudo: 'unknown',
+      firstname: "",
+      lastname: "",
+      email: "",
+      code_color: "",
     },
   }),
   actions: {
-    setUser(pseudo) {
-      this.user.pseudo = pseudo;
+    persistToLocalStorage() {
+      localStorage.setItem("user", JSON.stringify(this.user));
+    },
+    setUser(user) {
+      console.log("setUser called with:", user);
+      if (
+        user.id &&
+        user.firstname &&
+        user.lastname &&
+        user.email &&
+        user.code_color
+      ) {
+        this.user = {
+          id: user.id,
+          firstname: user.firstname,
+          lastname: user.lastname,
+          email: user.email,
+          code_color: user.code_color,
+        };
+        this.persistToLocalStorage();
+        console.log("Updated user state:", this.user);
+      }
+    },
+    $reset() {
+      this.$state.user = {
+        id: null,
+        firstname: "",
+        lastname: "",
+        email: "",
+        code_color: "",
+      };
+      localStorage.removeItem("user");
     },
   },
 });
