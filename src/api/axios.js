7c420b5a965d21ab88aff3496cc881c40a4927f6
Mohamed Zerrouki <mohamed.zerrouki@oclock.school>
2024-05-30 21:43:04 +0200

    feat: token handling

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,20 @@
 import axios from 'axios';
+import handleTokenExpiry from '@/utils/handleTokenExpiry';
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'http://localhost:4000';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+// Add a request interceptor
+axios.interceptors.response.use(
+  (response) => response, // return response if it's not an error
+  (error) => {
+    // Check if the error is due to an expired token
+    if (error.response.status === 401) {
+      handleTokenExpiry();
+    }
+    return Promise.reject(error); // return the error if it's not an expired token
+  },
+);
 
 export default axios;
