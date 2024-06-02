1ecbbbd812af22a2c44e87bc345aa99d191fc80a
Kheang-TE <kheang.te07@gmail.com>
2024-06-02 08:12:38 +0200

    feat: create and save new tag

--- src/components/BoardHeader.vue
+++ src/components/BoardHeader.vue
@@ -1,11 +1,12 @@
 <template>
 
-   <b-navbar fixed="top">
+  <b-navbar fixed="top">
     <b-navbar-brand>
       <b-button v-b-toggle.sidebar><font-awesome-icon :icon="['fas', 'bars']" /></b-button>
       <img src="/img/unity_manager_logo.webp" alt="Unity Manager" title="Unity Manager" class="logo">
     </b-navbar-brand>
     <b-navbar-nav class="ml-auto">
+      <Tag />
       <DropdownUser />
       <b-button v-b-toggle.chat><font-awesome-icon :icon="['fas', 'comments']" /></b-button>
     </b-navbar-nav>
@@ -19,11 +20,13 @@
 import DropdownUser from '@/components/boardComponents/DropdownUser.vue';
 import Sidebar from '@/components/boardComponents/Sidebar.vue';
 import Chat from '@/components/Chat.vue';
+import Tag from '@/components/boardComponents/Tag.vue';
 
 export default {
   name: 'BoardHeader',
   components: {
     Chat,
+    Tag,
     DropdownUser,
     Sidebar,
   },
