5154c82854e83dc32d6db2c009694ef6762f2751
Mohamed Zerrouki <mohamed.zerrouki@oclock.school>
2024-08-29 11:49:20 +0200

    feat: Reduced the number of events emitted to minimize unnecessary traffic

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import handleTokenExpiry from '@/utils/handleTokenExpiry';
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.baseURL = 'http://localhost:4000/api';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 // Add a request interceptor
